c3ab06501078bd9d9b105e17f3b200b1a1aab77a
gayoung0316 <baekmoon1230@gmail.com>
2021-08-22 23:14:08 +0900

    디버그 표시 제거

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return ScreenUtilInit(
       builder: () => MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
         theme: ThemeData(
           primarySwatch: Colors.blue,
